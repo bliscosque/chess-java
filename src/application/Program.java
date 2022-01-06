281ff2292613f0e0580048ca0dbe4ec47b66404d
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-01-06 10:38:41 -0600

    melhorando promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
+
 		while (!chessMatch.isCheckMate()) {
 			try {
 				UI.clearScreen();
@@ -36,9 +36,14 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				if(chessMatch.getPromoted() !=null) {
-					System.out.println("Enter piece for promotion - B/N/R/Q: ");
-					String type = sc.nextLine();
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion - B/N/R/Q: ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value: Enter piece for promotion - B/N/R/Q: ");
+						type = sc.nextLine().toUpperCase();
+					}
+
 					chessMatch.replacePromotedPiece(type);
 				}
 			} catch (ChessException e) {
